45e30c8db59f59eaec5e9f6d0bd5ab63511d2e33
Benny Louie <bennylouie1412@gmail.com>
2019-06-13 18:58:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 
+function destrictivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
